97fdff738d40ca0b7302f8eb2744b39d08a1c1ab
Andi Arapi <128141757+it1-amajor@users.noreply.github.com>
2024-01-10 19:57:49 +0100

    FIX CSS

--- data/data.js
+++ data/data.js
@@ -1,31 +1,29 @@
-esitiFlow = [
-    {
-        "esito": "In Pericolo devi Bypassare",
-        "descirizione": "Devi formarti te",
-        "isFinale": true
-    }
-];
+    esitiFlow = [
+        {
+            "esito": "Pericolo di bypassare portano a problematiche di gestione delle persone. La strategia organizzativa parte dall'osservare il tuo profilo e comprendere quali abitudini cambiare. Non prendere iniziativa per il proprio miglioramento mette in pericolo la tua azienda senza che tu te ne renda conto DEVI FORMARE TE STESSO",
+        }
+    ];
 
-flowChart = [
-    {
-        "domanda": "Oggi va bene? Ordinato?",
-        "si": 10,
-        "no": 1
-    },
-    {
-        "domanda": "Da piu' di 3 Mesi?",
-        "si": 4,
-        "no": 3
-    },
-    {
-        "domanda": "Hai Rappresentanti con te?",
-        "si": 5,
-        "no": 6
-    },
-    {
-        "domanda": "Sei Intervenuto?",
-        "si": 2,
-        "no": esitiFlow[0]
-    },
-];
+    flowChart = [
+        {
+            "domanda": "Oggi va bene? Ordinato?",
+            "si": 10,
+            "no": 1
+        },
+        {
+            "domanda": "Da piu' di 3 Mesi?",
+            "si": 4,
+            "no": 3
+        },
+        {
+            "domanda": "Hai Rappresentanti con te?",
+            "si": 5,
+            "no": 6
+        },
+        {
+            "domanda": "Sei Intervenuto?",
+            "si": 2,
+            "no": esitiFlow[0]
+        },
+    ];
 
