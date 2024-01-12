a1333fdbb2d6f8c0bfefa24be357666cf2d87612
Andi Arapi <128141757+it1-amajor@users.noreply.github.com>
2024-01-12 12:55:33 +0100

    fix flow

--- data/data.js
+++ data/data.js
@@ -35,72 +35,77 @@ const esitiFlow = [
 flowChart = [
     {   //0
         "domanda": "La situazione del venduto degli ultimi 9 mesi, rispetto alle tue aspettative, la ritieni positiva e in linea pienamente con il budget?",
-        "si": 6,
-        "no": 1
+        "si": 7,
+        "no": 1,
     },
     {   //1
         "domanda": "La situazione non positiva (o non come da aspettativa) è presente da più di 3 Mesi?",
         "si": 2,
-        "no": 6,
+        "no": 3,
     },
     {   //2
         "domanda": "Sei intervenuto direttamente per rimediare alla situazione?",
-        "si": 6,//-> 3 ???
+        "si": 3,
         "no": esitiFlow[1],
     },
     {   //3
-        "domanda": "La tua area commerciale ha già attivato un piano di lavoro chiaro per determinare risultati più ottimali?",
+        "domanda":  "Hai un reparto commerciale strutturato con una prima linea (che non sei tu) che ha la responsabilità sulle vendite?",
         "si": 4,
-        "no": esitiFlow[1]
-
+        "no": 13,
     },
     {   //4
-        "domanda": "Hai un sistema di monitoraggio dei risultati del venduto e dei sottoprodotti che lo compongono per comprendere i punti deboli del processo?",
+        "domanda": "La tua area commerciale ha già attivato un piano di lavoro chiaro per determinare risultati più ottimali?",
         "si": 5,
-        "no": esitiFlow[2]
+        "no": esitiFlow[1],
 
-    }, { //5
+    },
+    {   //5
+        "domanda": "Hai un sistema di monitoraggio dei risultati del venduto e dei sottoprodotti che lo compongono per comprendere i punti deboli del processo?",
+        "si": 6,
+        "no": esitiFlow[2],
+
+    }, { //6
         "domanda": "Le statistiche del venduto, dopo l'attuazione del piano di lavoro, stanno migliorando?",
         "si": esitiFlow[0],
-        "no": esitiFlow[2]
+        "no": esitiFlow[2],
 
     },
-    {   //6
-        "domanda":  "Hai un reparto commerciale strutturato con una prima linea (che non sei tu) che ha la responsabilità sulle vendite?",
-        "si": 7,
-        "no": 12
-    },
     {   //7
-        "domanda": "Il tuo reparto commerciale è così autonomo da non dover intervenire mai all'interno?",
-        "si": 9,
-        "no": 8
+        "domanda":  "Hai un reparto commerciale strutturato con una prima linea (che non sei tu) che ha la responsabilità sulle vendite?",
+        "si": 8,
+        "no": 13,
     },
     {   //8
-        "domanda": "Tutte le persone che compongono il reparto commerciale performano, sono alineate e crescono?",
-        "si": esitiFlow[2],
-        "no": esitiFlow[1]
+        "domanda": "Il tuo reparto commerciale è così autonomo da non dover intervenire mai all'interno?",
+        "si": 10,
+        "no": 9,
     },
     {   //9
-        "domanda": "Il tuo responsabile vendite ha una strategia commerciale chiara e che ha condiviso con il team?",
-        "si": 10,
-        "no": esitiFlow[4]
+        "domanda": "Tutte le persone che compongono il reparto commerciale performano, sono alineate e crescono?",
+        "si": esitiFlow[2],
+        "no": esitiFlow[1],
     },
     {   //10
-        "domanda": "L'area commerciale conosce bene la concorrenza e sà come andare a prendere altre quote di mercato?",
+        "domanda": "Il tuo responsabile vendite ha una strategia commerciale chiara e che ha condiviso con il team?",
         "si": 11,
-        "no": esitiFlow[2]
+        "no": esitiFlow[4],
     },
     {   //11
+        "domanda": "L'area commerciale conosce bene la concorrenza e sà come andare a prendere altre quote di mercato?",
+        "si": 12,
+        "no": esitiFlow[2],
+    },
+    {   //13
         "domanda": "Il budget 2023 che ti eri dato è stato raggiunto ?",
         "si": esitiFlow[0],
         "no": esitiFlow[1]
     },
-    {   //12
+    {   //13
         "domanda": "Hai già provato ad inserire un commerciale prima d'ora?",
-        "si": 13,
-        "no": esitiFlow[3]
+        "si": 14,
+        "no": esitiFlow[3],
     },
-     {  //13
+     {  //14
         "domanda": "Ritieni di avere sbagliato la scelta della persona?",
         "si": esitiFlow[3],
         "no": esitiFlow[4]
